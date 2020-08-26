d42e21448a301595808425cc0ca3ef4ca356de02
Kim Nguyen <nnhk23@gmail.com>
2020-08-26 23:46:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function dwarfRollCall(dwarves) {
+function dwarfRollCall(name) {
+  var str = "";
+  for (let i = 0; i < name.length; i++) {
+    str += `${(i+1)}. ${name[i]} `
+  }
+  return str
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(planet){
+  var arr = [];
+  for (let i = 0; i < planet.length; i++){
+    arr.push(planet[i].toUpperCase() + "!")
+  }
+  return arr
 }
 
 function longPlaneteerCalls(words) {
+  var empty = "";
+  for (let i = 0; i < words.length; i++){
+    empty = `${words[i]}`;
+    if (empty.length <= 4){
+      return false
+    } else {
+      return true
+    }
+  }
 }
 
 function findTheCheese (foods) {
-}
+  for (let i = 0; i < foods.length; i++){
+    if (foods[i] == "cheddar") {
+      return "cheddar";
+    } else if (foods[i] == "gouda"){
+      return "gouda";
+    } else if (foods[i] == "camembert"){
+      return "camembert";
+    }
+    }
+    return "no cheese!"
+  }
